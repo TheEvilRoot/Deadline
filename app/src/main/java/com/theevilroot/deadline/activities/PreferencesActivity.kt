25e5ebdacdabc3871819a2a697021dd5b618940a
TheEvilRoot <robot-creatix@ya.ru>
2018-07-18 08:17:30 +0300

    Backup before Windows reinstallation =)

--- app/src/main/java/com/theevilroot/deadline/activities/PreferencesActivity.kt
+++ app/src/main/java/com/theevilroot/deadline/activities/PreferencesActivity.kt
@@ -2,11 +2,16 @@ package com.theevilroot.deadline.activities
 
 import android.os.Build
 import android.os.Bundle
+import android.preference.PreferenceFragment
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
+import android.widget.FrameLayout
 import com.theevilroot.deadline.*
 import com.theevilroot.deadline.adapters.PreferencesAdapter
 import com.theevilroot.deadline.utils.bind
@@ -21,9 +26,10 @@ import java.io.File
 
 class PreferencesActivity: AppCompatActivity() {
     val layout = R.layout.preferences_activity
+
     private val toolBar: Toolbar by bind(R.id.toolbar)
-    private val preferenceList: RecyclerView by bind(R.id.preferences_list)
-    lateinit var adapter: PreferencesAdapter
+    private val container: FrameLayout by bind(R.id.preferences_container)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout)
@@ -35,28 +41,13 @@ class PreferencesActivity: AppCompatActivity() {
             window.statusBarColor = Colors.backgroundColor()
         }
         supportActionBar!!.title = "Настройки"
-        preferenceList.layoutManager = LinearLayoutManager(this)
-        adapter = PreferencesAdapter()
-        preferenceList.adapter = adapter
+        fragmentManager.beginTransaction().add(R.id.preferences_container, PreferencesFragment()).commit()
     }
-    override fun onDestroy() {
-        super.onDestroy()
-        savePreferences()
-    }
-    private fun savePreferences() {
-        val file = File(filesDir, "preferences")
-        if(!file.exists()) // WTF???
-            file.createNewFile()
-        writePreferences(file)
+    class PreferencesFragment: PreferenceFragment() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            addPreferencesFromResource(R.xml.app_preferences)
+        }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            android.R.id.home -> {
-                TheHolder.needRefresh = true
-                this.finish()
-            }
-        }
-        return true
-    }
 }
